eafc8da559e6c24f130c2b915e0483d3f686a6b5
Chase Cannon <chase27cannon52@gmail.com>
2019-09-04 14:01:23 -0500

    html and css

---
+++ assets/javascript/giftastic.js
@@ -0,0 +1,3 @@
+// Topics variable
+var shows = ["Daredevil", "Breaking Bad", "The Office", "Game of Thrones", "Fresh Prince of Bel-Air", "Stranger Things", "Parks and Recreation"];   
+function renderButtons()
